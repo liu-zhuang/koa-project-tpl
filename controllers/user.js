82afb1b691036fd270d3d87781e33c86c48da5b0
nickylau82 <70458055@qq.com>
2017-09-25 22:47:03 +0800

    modify

--- controllers/user.js
+++ controllers/user.js
@@ -1,31 +1,33 @@
 const devLog = require('debug')('dev'); // debugger的namespace是dev 命令行里DEBUG=dev或者*的时候才会输出
 const errLog = require('debug')('err'); //错误日志
 const logger = require('../util/loggerUtil');
-const proxy = require('./proxy');
+const axios = require('axios');
 
 const getUser = async (ctx, next) => {
-	await proxy.test().then(data => {
-		devLog(data);
-		ctx.response.type = 'application/xml';
-		ctx.response.body = '<xm><data>1</data></xml>';
-		return next();
+	await axios.get('https://api.github.com/users/' + ctx.params.userName)
+	.then(res => {
+		if (res.status === 200 && res.data) {
+			ctx.body = res.data;
+		}
 	});
-	// ctx.response.body = 'aaa';
 }
 
 const getList = async (ctx, next) => {
 	debugger;
-	ctx.response.type = 'application/json';
-	ctx.response.body = [{
-		name: 'pangwang',
-		age: 25
-	}, {
-		name: 'mff',
-		age: 18
-	}]
+	await axios.get('https://api.github.com/users', {
+		params: {
+			since: ctx.params.page
+		}
+	})
+	.then(res => {
+		if (res.status === 200 && res.data) {
+			devLog(res.data);
+			ctx.body = res.data;
+		}
+	})
 }
 
 module.exports = {
 	'Get /user/:userName': getUser,
-	'Get /user': getList
+	'Get /users/:page': getList
 }
